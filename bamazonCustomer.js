8cdd6580ad1dacc5956e960f04580b1283674651
Garrett Griffin <griffin.garrett@gmail.com>
2019-05-08 17:22:07 -0500

    table console.logs on connection

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,4 +12,15 @@ const connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw (err);
     console.log('connected as id ' + connection.threadId);
+    queryAllProducts();
 });
+
+function queryAllProducts() {
+    connection.query('SELECT * FROM products', function(err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].item_id + ' | ' + res[i].product_name + ' | '
+             + res[i].department_name + ' | ' + res[i].price + ' | ' +
+             res[i].stock_quantity)
+        }
+    });
+}
