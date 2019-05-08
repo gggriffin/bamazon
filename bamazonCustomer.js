ec7d644766ca2c05c790e56496643361fcb63420
Garrett Griffin <griffin.garrett@gmail.com>
2019-05-08 17:01:38 -0500

    connected js file to mySQL table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,15 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'bamazon'
+});
+
+connection.connect(function(err) {
+    if (err) throw (err);
+    console.log('connected as id ' + connection.threadId);
+});
